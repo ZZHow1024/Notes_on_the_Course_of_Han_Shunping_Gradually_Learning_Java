6ddddd4cbcb9647792e660aeda8cd846767ce846
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-16 18:30:30 +0800

    Add files via upload

---
+++ Chapter01/0016_快速入门/Hello.java
@@ -0,0 +1,16 @@
+//DNX写于2024年1月16日
+//这是Java的快速入门，展示Java的开发步骤
+
+//1.public class Hello 表示Hello是一个类，是一个public公有的类
+//2.Hello{ } 表示一个类的开始和结束
+//3.public static void main(String[] args) 表示一个主方法，即我们程序的入口
+//4.main(){ } 表示方法的开始和结束
+//5.System.out.println("Hello World!"); 表示输出"Hello World!"到屏幕
+//6. ;表示语句结束
+public class Hello{
+
+	//编写一个main方法
+	public static void main(String[] args){
+			System.out.println("Hello World!");
+	}
+}
