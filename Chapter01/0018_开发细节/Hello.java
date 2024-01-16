e3ee18845e686eec611bf1206aadce4e4cb8a7e8
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-16 18:31:43 +0800

    Add files via upload

---
+++ Chapter01/0018_开发细节/Hello.java
@@ -0,0 +1,24 @@
+//DNX写于2024年1月16日
+public class Hello{
+
+	public static void main(String[] args){
+
+		System.out.println("Hello!");
+	}
+}
+
+class Dog{
+	
+	public static void main(String[] args){
+
+		System.out.println("Dog!");
+	}
+}
+
+class Cat{
+
+	public static void main(String[] args){
+
+		System.out.println("Cat!");
+	}
+}
