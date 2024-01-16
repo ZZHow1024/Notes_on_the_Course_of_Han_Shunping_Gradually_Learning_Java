f2d55a60aac69b60cc921c1bc19d8acc2161684f
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-16 18:35:52 +0800

    Add files via upload

---
+++ Chapter01/0023_注释/Comment.java
@@ -0,0 +1,18 @@
+/**
+ * @author DNX
+ * @version 1.0
+ */
+
+//DNX写于2024年1月16日
+public class Comment{
+
+	public static void main(String[] args){
+
+		System.out.println("Comment!");
+		/*
+		  多行注释
+		  多行注释
+		  多行注释
+		*/
+	}
+}
