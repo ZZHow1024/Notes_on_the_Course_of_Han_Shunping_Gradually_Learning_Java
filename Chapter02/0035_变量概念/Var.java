cc45ce34e0a06d2be549abe177895fdbf06a1ba4
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-18 13:54:57 +0800

    Add files via upload

---
+++ Chapter02/0035_变量概念/Var.java
@@ -0,0 +1,16 @@
+//DNX写于2024年1月16日
+public class Var{
+	public static void main(String[] args){
+		
+		//声明变量
+		int a;
+		//赋值
+		a = 100;
+		//使用
+		System.out.println(a);
+
+		//还可以这样使用
+		int b = 800;
+		System.out.println(b);
+	}
+}
