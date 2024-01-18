d65e37e0c0ae21848f88daafc7f77f4dc36f7a37
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-18 18:03:28 +0800

    Add files via upload

---
+++ Chapter03_变量/0050_布尔类型/Boolean.java
@@ -0,0 +1,15 @@
+//DNX写于2024年1月17日
+public class Boolean{
+	public static void main(String[] args){
+
+		//演示判断成绩是否通过的案例
+		//定义一个布尔变量
+		boolean isPass = true;
+
+		if(isPass){
+			System.out.println("考试通过，恭喜！");
+		}else{
+			System.out.println("考试没有通过，下次努力！");
+		}
+	}
+}
