0cc1797309c41dfcbfc196e181c5716a5b2285bd
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-18 14:00:48 +0800

    Add files via upload

---
+++ Chapter02/0041_整型细节/IntDetail.java
@@ -0,0 +1,13 @@
+//DNX写于2024年1月16日
+public class IntDetail{
+	public static void main(String[] args){
+
+		//Java的整型常量 (具体值) 默认为 int 型，声明long型常量须后加 ’l’ 或 ’L’。
+		int n1 = 1;  //4个字节。
+	  //int n2 = 1L; //×
+		long n3 = 1L;//√
+
+		System.out.println("n1 = " + n1);
+		System.out.println("n3 = " + n3);
+	}
+}
