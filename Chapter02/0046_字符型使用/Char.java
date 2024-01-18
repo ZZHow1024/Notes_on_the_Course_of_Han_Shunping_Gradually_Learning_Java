a74d1ce2235458548217dfc65ce4e6af05979c51
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-18 14:03:51 +0800

    Add files via upload

---
+++ Chapter02/0046_字符型使用/Char.java
@@ -0,0 +1,14 @@
+//DNX写于2024年1月17日
+public class Char{
+	public static void main(String[] args){
+		char c1 = 'a';
+		char c2 = '\t';
+		char c3 = '人';
+		char c4 = 98; //说明：字符类型可以直接存放一个数字
+
+		System.out.println("c1 = " + c1);
+		System.out.println("c2 = " + c2);
+		System.out.println("c3 = " + c3);
+		System.out.println("c4 = " + c4);//当输出c4时，会输出97表示的字符 --> 编码的概念
+	}
+}
