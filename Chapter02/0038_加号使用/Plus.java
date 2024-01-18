563a785d228f953ed42cfddf0d0a8d736f6b9e5f
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-18 13:57:38 +0800

    Add files via upload

---
+++ Chapter02/0038_加号使用/Plus.java
@@ -0,0 +1,10 @@
+//DNX写于2024年1月16日
+public class Plus{
+	public static void main(String[] args){
+		System.out.println(100 + 98);//198
+		System.out.println("100" + 98);//10098
+
+		System.out.println(100 + 3 + "Hello");//103Hello
+		System.out.println("Hello" + 100 + 3);//Hello1003
+	}
+}
