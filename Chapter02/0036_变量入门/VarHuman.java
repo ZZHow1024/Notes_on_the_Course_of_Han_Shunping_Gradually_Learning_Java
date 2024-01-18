41812f40a422630a153018f5d3015e5f9949eb3e
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-18 13:55:44 +0800

    Add files via upload

---
+++ Chapter02/0036_变量入门/VarHuman.java
@@ -0,0 +1,17 @@
+//DNX写于2024年1月16日
+public class VarHuman{
+	//编写一个main方法
+	public static void main(String[] args){
+		//记录人的信息
+		int age = 10;
+		double score = 99.9;
+		char gender = '♂';
+		String name = "DNX";
+		//输出信息
+		System.out.println("人的信息如下：");
+		System.out.println("姓名：" + name);
+		System.out.println("性别：" + gender);
+		System.out.println("年龄：" + age);
+		System.out.println("成绩：" + score);
+	}
+}
