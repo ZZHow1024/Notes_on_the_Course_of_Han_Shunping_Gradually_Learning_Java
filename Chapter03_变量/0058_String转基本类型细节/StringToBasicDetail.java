b10dd1658da5a7dee125c7042d2c1684acd4014c
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-19 21:42:51 +0800

    Add files via upload

---
+++ Chapter03_变量/0058_String转基本类型细节/StringToBasicDetail.java
@@ -0,0 +1,15 @@
+//DNX写于2024年1月18日
+public class StringToBasicDetail{
+	public static void main(String[] args){
+
+		//正常 String -> int
+		String str1 = "123";
+		int n1 = Integer.parseInt(str1);
+		System.out.println(n1);
+
+		//会出异常的 String -> int
+		String str2 = "hello";
+		int n2 = Integer.parseInt(str2);
+		System.out.println(n2);
+	}
+}
