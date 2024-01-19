b8fda8c503406e9fb22b3da52d6f5c7cff62c797
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-19 21:18:49 +0800

    Add files via upload

---
+++ Chapter03_变量/0051_自动类型转换基础/AutoConvert.java
@@ -0,0 +1,10 @@
+//DNX写于2024年1月17日
+public class AutoConvert{
+	public static void main(String[] args){
+		//演示自动转换
+		int num = 'a';//  √ char -> int
+		double d1 = 80;// √ int  -> double
+		System.out.println(num);// 97
+		System.out.println(d1); // 80
+	}
+}
