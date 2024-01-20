ee464e016a250a90e51e3426748fa12726823c0c
DNX <109335896+dnx00@users.noreply.github.com>
2024-01-20 19:59:08 +0800

    Add files via upload

---
+++ Chapter04_运算符/0086_四种进制介绍/BinaryTest.java
@@ -0,0 +1,21 @@
+//DNX写于2024年1月19日
+public class BinaryTest{
+	public static void main(String[] args){
+
+		//在Java中四种进制的表示方法
+
+		//二进制
+		int n1 = 0B1010;
+		//十进制
+		int n2 = 1010;
+		//八进制
+		int n3 = 01010;
+		//十六进制
+		int n4 = 0X1010;
+
+		System.out.println("0B1010 = " + n1);
+		System.out.println("1010 = " + n2);
+		System.out.println("01010 = " + n3);
+		System.out.println("0X1010 = " + n4);
+	}
+}
