409cce4fecca5ae431c4d32175b2d54123ee94ea
DNX <109335896+dnx00@users.noreply.github.com>
2024-03-25 22:33:07 +0800

    Create DateTimeFormatter_.java

---
+++ Chapter13_常用类/src/com/dnx/date_/DateTimeFormatter_.java
@@ -0,0 +1,23 @@
+package com.dnx.date_;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 2024年3月25日
+ *
+ * @author DNX
+ */
+public class DateTimeFormatter_ {
+    public static void main(String[] args) {
+        //案例：第三代日期使用 DateTimeFormatter 进行格式化
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt);
+
+        System.out.println("===格式化后===");
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss");
+        String format = dateTimeFormatter.format(ldt);
+        System.out.println(format);
+    }
+}
