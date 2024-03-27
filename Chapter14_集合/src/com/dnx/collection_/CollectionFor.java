0c5891e1eecba131fa9ea00ae633d00cb452cdb4
DNX <109335896+dnx00@users.noreply.github.com>
2024-03-27 16:19:32 +0800

    Create CollectionFor.java

---
+++ Chapter14_集合/src/com/dnx/collection_/CollectionFor.java
@@ -0,0 +1,37 @@
+package com.dnx.collection_;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * 2024年3月27日
+ *
+ * @author DNX
+ */
+@SuppressWarnings("all")
+public class CollectionFor {
+    public static void main(String[] args) {
+        //案例：增强 for 循环演示
+        Collection list = new ArrayList();
+
+        list.add(new Book("Discrete mathematics", "Jack", 50.00));
+        list.add(new Book("Linear algebra", "Tom", 60.50));
+        list.add(new Book("Data structure", "Jerry", 12.35));
+
+        System.out.println("list = " + list + "\n");
+
+        //增强 for 在 Collection 集合使用
+        //在 Collection 集合使用的增强 for 底层仍然是迭代器
+        //证明：Debug 时使用 Force Step Into
+        for (Object book : list) {
+            System.out.println("book = " + book);
+        }
+        System.out.println();
+
+        //增强 for 也可以用在数组上
+        int[] nums = {1, 3, 5};
+        for (int i : nums) {
+            System.out.println("i = " + i);
+        }
+    }
+}
