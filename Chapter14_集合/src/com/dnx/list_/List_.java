057f40aae164c207f1180c80f55fee547a11b64f
DNX <109335896+dnx00@users.noreply.github.com>
2024-03-29 23:39:38 +0800

    Create List_.java

---
+++ Chapter14_集合/src/com/dnx/list_/List_.java
@@ -0,0 +1,30 @@
+package com.dnx.list_;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 2024年3月29日
+ *
+ * @author DNX
+ */
+@SuppressWarnings("all")
+public class List_ {
+    public static void main(String[] args) {
+        //List 集合类中元素有序(即添加顺序和取出顺序一致)、且可重复。
+        System.out.println("1. List 集合类中元素有序(即添加顺序和取出顺序一致)、且可重复。");
+        List list = new ArrayList();
+        list.add("Tom");
+        list.add("Jack");
+        list.add("Mike");
+        list.add("Jack");
+        list.add("Tom");
+
+        System.out.println("list = " + list + "\n");
+
+        //List 集合中的每个元素都有其对应的顺序索引，即支持索引。
+        //  索引从0开始
+        System.out.println("2. List 集合中的每个元素都有其对应的顺序索引，即支持索引。");
+        System.out.println("list.get(2) = " + list.get(2));
+    }
+}
