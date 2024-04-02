198ef344d1aaedcb4c06c45e6a94f1cb4fafa11e
DNX <109335896+dnx00@users.noreply.github.com>
2024-04-02 12:12:01 +0800

    Create ListFor.java

---
+++ Chapter14_集合/src/com/dnx/list_/ListFor.java
@@ -0,0 +1,44 @@
+package com.dnx.list_;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 2024年3月30日
+ *
+ * @author DNX
+ */
+@SuppressWarnings("all")
+public class ListFor {
+    public static void main(String[] args) {
+        //案例：List三种遍历方式
+        List list = new ArrayList();
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+
+        //遍历
+        //1. 迭代器
+        System.out.println("1. 迭代器");
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Object next = iterator.next();
+            System.out.println(next);
+        }
+        System.out.println();
+
+        //2. 增强 for
+        System.out.println("2. 增强 for");
+        for(Object o : list){
+            System.out.println(o);
+        }
+        System.out.println();
+
+        //3. 普通 for
+        System.out.println("3. 普通 for");
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
+}
