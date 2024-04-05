8995bd9860adeb7acc38fbdea143225cbab4c64e
DNX <109335896+dnx00@users.noreply.github.com>
2024-04-05 23:59:25 +0800

    Create SetMethod.java

---
+++ Chapter14_集合/src/com/dnx/set_/SetMethod.java
@@ -0,0 +1,43 @@
+package com.dnx.set_;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * 2024年4月5日
+ *
+ * @author DNX
+ */
+@SuppressWarnings("all")
+public class SetMethod {
+    public static void main(String[] args) {
+        //案例：Set 接口的方法
+        //以 HashSet 为例
+        Set set = new HashSet();
+        set.add("Tom");
+        set.add("Jerry");
+        set.add("Jerry"); //重复
+        set.add("Spike");
+        set.add(null);
+        set.add(null); //重复
+
+        System.out.println("set = " + set);
+
+        //遍历：
+        //方式1 迭代器
+        System.out.println("遍历方式1 迭代器");
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()) {
+            Object next = iterator.next();
+            System.out.println(next);
+        }
+        System.out.println();
+
+        //方式2 增强 for 循环
+        System.out.println("遍历方式2 增强 for 循环");
+        for (Object o : set) {
+            System.out.println(o);
+        }
+    }
+}
