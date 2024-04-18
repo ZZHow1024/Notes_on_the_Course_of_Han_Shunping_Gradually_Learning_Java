70b1c01ea79f739b8af84a347e554117d5c1147e
DNX <109335896+dnx00@users.noreply.github.com>
2024-04-18 19:49:59 +0800

    Create TreeSet_.java

---
+++ Chapter14_集合/src/com/dnx/set_/TreeSet_.java
@@ -0,0 +1,48 @@
+package com.dnx.set_;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+/**
+ * 2024年4月18日
+ *
+ * @author DNX
+ */
+@SuppressWarnings("all")
+public class TreeSet_ {
+    public static void main(String[] args) {
+        //案例：按字符串字典序排序 和 按字符串长度排序
+        //treeSet1 按字符串字典序排序
+        System.out.println("treeSet1 按字符串字典序排序");
+        TreeSet treeSet1 = new TreeSet(new Comparator<Object>() {
+            public int compare(Object o1, Object o2) {
+                return ((String) o1).compareTo((String) o2);
+            }
+        });
+
+        treeSet1.add("Camera");
+        treeSet1.add("Alfa");
+        treeSet1.add("Banana");
+        treeSet1.add("Cinema");
+        treeSet1.add("Dog");
+
+        System.out.println("treeSet1 = " + treeSet1 + "\n");
+
+        //treeSet2 按字符串长度(从短到长)排序
+        System.out.println("treeSet2 按字符串长度(从短到长)排序");
+        TreeSet treeSet2 = new TreeSet(new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((String) o1).length() - ((String) o2).length();
+            }
+        });
+
+        treeSet2.add("abc");
+        treeSet2.add("def");
+        treeSet2.add("a");
+        treeSet2.add("abcde");
+        treeSet2.add("vwxyz");
+
+        System.out.println("treeSet2 = " + treeSet2);
+    }
+}
