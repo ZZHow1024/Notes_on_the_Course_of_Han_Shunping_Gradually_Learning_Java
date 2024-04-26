1110e887aaaa3973e3b843cc46f801475610a9ce
ZZHow1024 <109335896+ZZHow1024@users.noreply.github.com>
2024-04-26 19:33:18 +0800

    Create JUnit_.java

---
+++ Chapter15_泛型/src/com/zzhow/junit_/JUnit_.java
@@ -0,0 +1,24 @@
+package com.zzhow.junit_;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 2024年4月26日
+ *
+ * @author ZZHow
+ */
+public class JUnit_ {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void m1() {
+        System.out.println("m1~");
+    }
+
+    @Test
+    public void m2() {
+        System.out.println("m2~");
+    }
+}
