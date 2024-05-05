97595d886b81872585dce0ea8d3004d53fe07de6
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-05-05 23:29:36 +0800

    Create Bomb.java

---
+++ Chapter18_坦克大战_2/src/zzhow/tankgame/Bomb.java
@@ -0,0 +1,67 @@
+package zzhow.tankgame;
+
+/**
+ * 2024/5/5
+ *
+ * @author ZZHow
+ */
+public class Bomb {
+    private int x; //炸弹横坐标
+    private int y; //炸弹纵坐标
+    private int life; //炸弹生命周期
+    private boolean isLive = true; //炸弹是否存活
+
+    public Bomb() {
+    }
+
+    public Bomb(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Bomb(int x, int y, int life, boolean isLive) {
+        this.x = x;
+        this.y = y;
+        this.life = life;
+        this.isLive = isLive;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public boolean isLive() {
+        return isLive;
+    }
+
+    public void setLive(boolean live) {
+        isLive = live;
+    }
+
+    public void lifeDown() {
+        if (life > 0)
+            --life;
+        else
+            isLive = false;
+    }
+}
