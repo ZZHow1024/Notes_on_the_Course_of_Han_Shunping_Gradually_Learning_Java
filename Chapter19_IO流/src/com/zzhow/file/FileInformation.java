176513b5f8b8b1bba694c2486921abceb86cb0cf
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-05-13 17:15:34 +0800

    Create FileInformation.java

---
+++ Chapter19_IO流/src/com/zzhow/file/FileInformation.java
@@ -0,0 +1,41 @@
+package com.zzhow.file;
+
+import java.io.File;
+
+/**
+ * 2024/5/13
+ *
+ * @author ZZHow
+ */
+public class FileInformation {
+    public static void main(String[] args) {
+        fileInfo();
+    }
+
+    //获取文件信息
+    public static void fileInfo() {
+        //创建文件对象
+        File file = new File("E:\\", "news.txt");
+
+        //getName() 获取文件名
+        System.out.println("文件名：" + file.getName());
+
+        //getAbsolutePath() 获取文件绝对路径
+        System.out.println("文件绝对路径：" + file.getAbsolutePath());
+
+        //getParent() 获取文件父级目录
+        System.out.println("文件父级目录：" + file.getParent());
+
+        //length() 获取文件大小(字节 byte)
+        System.out.println("文件大小(字节 byte)：" + file.length());
+
+        //exists() 判断文件是否存在
+        System.out.println("文件是否存在：" + file.exists());
+
+        //isFile() 判断是否是一个文件
+        System.out.println("是否是一个文件：" + file.isFile());
+
+        //isDirectory() 判断是否是一个目录
+        System.out.println("是否是一个目录：" + file.isDirectory());
+    }
+}
