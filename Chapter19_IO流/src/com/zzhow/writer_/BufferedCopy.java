9226aaee2d569e8427a42bd9e367c3775e523228
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-05-15 19:41:31 +0800

    Create BufferedCopy.java

---
+++ Chapter19_IO流/src/com/zzhow/writer_/BufferedCopy.java
@@ -0,0 +1,28 @@
+package com.zzhow.writer_;
+
+import java.io.*;
+
+/**
+ * 2024/5/15
+ *
+ * @author ZZHow
+ * 案例：综合使用 BufferedReader 与 BufferedWriter 进行文件拷贝
+ */
+public class BufferedCopy {
+    public static void main(String[] args) {
+        String srcFilePath = "D:\\hello.txt";
+        String destFilePath = "D:\\hello1.txt";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(srcFilePath));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(destFilePath))) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                bufferedWriter.write(line);
+                if ((line = bufferedReader.readLine()) != null)
+                    bufferedWriter.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
