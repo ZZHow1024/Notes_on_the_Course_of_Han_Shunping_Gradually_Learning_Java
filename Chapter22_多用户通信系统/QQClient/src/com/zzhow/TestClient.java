e5a40b9f772bee83116fd598b0b498c69b93b335
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-05-27 22:51:11 +0800

    Add: Test class

---
+++ Chapter22_多用户通信系统/QQClient/src/com/zzhow/TestClient.java
@@ -0,0 +1,16 @@
+package com.zzhow;
+
+import com.zzhow.qqclient.view.QQView;
+
+/**
+ * 2024/5/27
+ *
+ * @author ZZHow
+ * @Version 1.0
+ * 测试类
+ */
+public class TestClient {
+    public static void main(String[] args) {
+        new QQView().mainMenu();
+    }
+}
---
+++ Chapter22_多用户通信系统/QQServer/src/com/zzhow/TestServer.java
@@ -0,0 +1,14 @@
+package com.zzhow;
+
+import com.zzhow.qqserver.service.QQServer;
+
+/**
+ * 2024/5/
+ *
+ * @author ZZHow
+ */
+public class TestServer {
+    public static void main(String[] args) {
+        new QQServer();
+    }
+}
