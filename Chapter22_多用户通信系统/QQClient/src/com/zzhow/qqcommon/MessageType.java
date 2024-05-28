2dcf0b7107f890e3d56d43a7df322c2c33a7b910
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-05-28 12:17:49 +0800

    Update: MessageType.java

--- Chapter22_多用户通信系统/QQClient/src/com/zzhow/qqcommon/MessageType.java
+++ Chapter22_多用户通信系统/QQClient/src/com/zzhow/qqcommon/MessageType.java
@@ -1,13 +1,17 @@
 package com.zzhow.qqcommon;
 
 /**
- * 2024/5/27
+ * 2024/5/28
  *
  * @author ZZHow
- * @version 1.0
+ * @version 2.0
  * 消息类型
  */
 public interface MessageType {
     String MESSAGE_LOGIN_SUCCEEDED = "1"; //登录成功消息
     String MESSAGE_LOGIN_FAILED = "2"; //登录失败消息
+    String MESSAGE_COMMON = "3"; //普通信息包
+    String MESSAGE_GET_FRIEND = "4"; //客户端请求返回在线用户列表
+    String MESSAGE_RES_FRIEND = "5"; //服务器端返回在线用户列表
+    String MESSAGE_GET_FRIEND_LIST = "6"; //客户端请求登出
 }
