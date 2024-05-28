f1fe59f34509b22f64ef61d50df8684f532239c0
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-05-28 13:04:22 +0800

    Server Add: the response to MESSAGE_GET_FRIEND message

--- Chapter22_多用户通信系统/QQServer/src/com/zzhow/qqserver/service/ManageServerConnectClientThread.java
+++ Chapter22_多用户通信系统/QQServer/src/com/zzhow/qqserver/service/ManageServerConnectClientThread.java
@@ -1,6 +1,9 @@
 package com.zzhow.qqserver.service;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 2024/5/27
@@ -21,5 +24,21 @@ public class ManageServerConnectClientThread {
     //获取线程
     public static ServerConnectClientThread getServerConnectClientThread(String userID) {
         return hashMap.get(userID);
+    }
+
+    //获取在线用户列表(用,分割)
+    public static String getOnlineUsersList() {
+        StringBuilder onlineUsersList = new StringBuilder();
+
+        Iterator<String> iterator = hashMap.keySet().iterator();
+        while (true) {
+            onlineUsersList.append(iterator.next());
+            if (iterator.hasNext())
+                onlineUsersList.append(",");
+            else
+                break;
+        }
+
+        return onlineUsersList.toString();
     }
 }
