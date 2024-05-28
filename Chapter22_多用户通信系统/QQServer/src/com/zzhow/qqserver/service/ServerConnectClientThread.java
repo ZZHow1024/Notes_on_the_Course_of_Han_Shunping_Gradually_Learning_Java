889af717ddad2f3ff6b82fa164022cc94b4c1b05
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-05-28 13:04:48 +0800

    Server Add: getOnlineUsersList() method

--- Chapter22_多用户通信系统/QQServer/src/com/zzhow/qqserver/service/ServerConnectClientThread.java
+++ Chapter22_多用户通信系统/QQServer/src/com/zzhow/qqserver/service/ServerConnectClientThread.java
@@ -1,9 +1,11 @@
 package com.zzhow.qqserver.service;
 
 import com.zzhow.qqcommon.Message;
+import com.zzhow.qqcommon.MessageType;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /**
@@ -29,6 +31,23 @@ public class ServerConnectClientThread extends Thread {
                 System.out.println("等待接收客户端的数据");
                 ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
                 Message message = (Message) objectInputStream.readObject();
+
+                switch (message.getMessageType()) {
+                    case MessageType.MESSAGE_GET_FRIEND -> {
+                        System.out.println("客户端 " + message.getSender() + " 请求拉取在线用户列表");
+
+                        //构建 Message 对象
+                        Message onlineUsersListMessage = new Message();
+                        onlineUsersListMessage.setMessageType(MessageType.MESSAGE_RES_FRIEND);
+                        onlineUsersListMessage.setReceiver(message.getSender());
+                        onlineUsersListMessage.setContent(ManageServerConnectClientThread.getOnlineUsersList());
+
+                        //返回给客户端
+                        ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+                        objectOutputStream.writeObject(onlineUsersListMessage);
+                    }
+                    case null, default -> System.out.println("暂不处理");
+                }
             } catch (IOException | ClassNotFoundException e) {
                 System.out.println("异常信息：" + e.getMessage());
                 break;
